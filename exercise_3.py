a281fb35cdd5f6ebc64649e7e34c8f6b0756a062
MiguelED <migueled2695@gmail.com>
2021-12-23 22:52:54 -0600

    Exercise

---
+++ exercise_3.py
@@ -0,0 +1,8 @@
+#Wirte a program that will produce the following output- all the next should appear in your program´s output
+#Number 1   The Larch
+#Number 2   The Horse Chestnut
+
+stringOne = "Number 1\tThe larch"
+stringTwo = 'Number 2\tThe hourse chestnut'
+
+print(stringOne + '\n' + stringTwo)
