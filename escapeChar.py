dd09728356e447f5009725652aa0447e22961df2
MiguelED <migueled2695@gmail.com>
2021-12-23 23:02:25 -0600

    More on escape

--- escapeChar.py
+++ escapeChar.py
@@ -8,7 +8,8 @@ print('The pet shop owner said "No, no \'e\'s uh,...he\'s resting".')
 #or
 print("The pet shop owner said \"No, no 'e's uh,...he's resting\".")
 #or
-print("""The pet shop owner said "No, no 'e's uh,...he's resting".""")
+print("""The pet shop owner said "No, no,\
+'e's uh,...he's resting".""")
 
 anotherSplitString = """This string has been
 split over \
@@ -16,3 +17,8 @@ several
 lines"""
 
 print(anotherSplitString)
+
+# print("C:\Users\tim\notes") error \t \n
+print("C:\\Users\\tim\\notes")
+#or
+print(r"C:\Users\tim\notes")
