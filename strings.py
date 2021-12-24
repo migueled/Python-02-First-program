ea24d58cc2b1aa2e5f505f0b323daadf756506c3
MiguelED <migueled2695@gmail.com>
2021-12-23 22:26:13 -0600

    input important

---
+++ strings.py
@@ -0,0 +1,10 @@
+print("today is a good day to learn puthon")
+print('python is fun')
+print("python' is string are easy to use")
+print('we can even include "quotes" in strings')
+print("hello" + " world")##concatenation
+
+# if we want a space, we can add that too
+greeting = "hello"
+name     = input("please ente your name ")
+print(greeting + ' ' + name)
